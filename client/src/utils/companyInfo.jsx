e0b5e8ee35405b40dc6c1631ce830c176f6523a0
omgameublement <og1731195@gmail.com>
2025-05-30 15:48:14 +0200

    first

---
+++ client/src/utils/companyInfo.jsx
@@ -0,0 +1,9 @@
+const companyInfo = {
+  name: "OMG AMEUBLEMENT",
+  ninea: "006396312",
+  address: "Rue 61 X 52 Gueule Tapee Dakar",
+  phone: "+221 77 651 37 74",
+  email: "og1731195@gmail.com",
+};
+
+export default companyInfo;
